d2e724d0acbc59b633989384ada1a011e21f8222
int mian <w923959419@gmail.com>
2019-10-26 21:28:47 +0800

    update datebase files

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -4,68 +4,122 @@ grant all on web.* to 'webUser'@'%';
 
 use web;
 
-CREATE TABLE IF NOT EXISTS `locationInfo`(
-   `locationId` INT UNSIGNED AUTO_INCREMENT,
-   `parentId` INT UNSIGNED NOT NULL,
-   `locationName` VARCHAR(100) NOT NULL,
-   `level` INT UNSIGNED NOT NULL,-- country,province,city,county
-   PRIMARY KEY ( `locationId` )
-)CHARSET=utf8;
+-- --------------------------------------------------
+-- --------------------------------------------------
+-- location
+-- --------------------------------------------------
+-- --------------------------------------------------
+DROP TABLE IF EXISTS `locationInfo`;
+CREATE TABLE `locationInfo`
+(
+    `locationId`   INT UNSIGNED AUTO_INCREMENT,
+    `parentId`     INT UNSIGNED NOT NULL,
+    `locationName` VARCHAR(100) NOT NULL,
+    `level`        INT UNSIGNED NOT NULL,-- country,province,city,county
+    PRIMARY KEY (`locationId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `userInfo`(
-   `userId` INT UNSIGNED AUTO_INCREMENT,
-   `username` VARCHAR(100) NOT NULL,
-   `password` VARCHAR(100) NOT NULL,
-   `phoneNumber` VARCHAR(100),
-   `nickname` VARCHAR(100) NOT NULL,
-   PRIMARY KEY ( `userId` )
-)CHARSET=utf8;
+-- --------------------------------------------------
+-- --------------------------------------------------
+-- user
+-- --------------------------------------------------
+-- --------------------------------------------------
+DROP TABLE IF EXISTS `userInfo`;
+CREATE TABLE `userInfo`
+(
+    `userId`      INT UNSIGNED AUTO_INCREMENT,
+    `username`    VARCHAR(100) NOT NULL,
+    `password`    VARCHAR(100) NOT NULL,
+    `phoneNumber` VARCHAR(100),
+    `nickname`    VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`userId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `addressInfo`(
-   `addressId` INT UNSIGNED AUTO_INCREMENT,
-   `userId` INT UNSIGNED NOT NULL,
-   `isDefault` INT NOT NULL,
-   `name` VARCHAR(100) NOT NULL,
-   `phoneNumber` VARCHAR(100) NOT NULL,
-   `locationId` INT UNSIGNED NOT NULL,  -- level4
-   `detailedAddress` VARCHAR(100) NOT NULL,
-   PRIMARY KEY ( `addressId` )
-)CHARSET=utf8;
+DROP TABLE IF EXISTS `addressInfo`;
+CREATE TABLE `addressInfo`
+(
+    `addressId`       INT UNSIGNED AUTO_INCREMENT,
+    `userId`          INT UNSIGNED NOT NULL,
+    `isDefault`       BOOLEAN      NOT NULL,
+    `name`            VARCHAR(100) NOT NULL,
+    `phoneNumber`     VARCHAR(100) NOT NULL,
+    `locationId`      INT UNSIGNED NOT NULL,-- level4
+    `detailedAddress` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`addressId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `category`(
-   `categoryId` INT UNSIGNED AUTO_INCREMENT,
-   `categoryName` VARCHAR(20) NOT NULL,
-   PRIMARY KEY ( `categoryId` )
-)CHARSET=utf8;
+-- --------------------------------------------------
+-- orders
+-- --------------------------------------------------
+DROP TABLE IF EXISTS `orderInfo`;
+CREATE TABLE `orderInfo`
+(
+    `orderId`       INT UNSIGNED AUTO_INCREMENT,
+    `userId`        INT UNSIGNED NOT NULL,
+    `orderDate`     DATETIME     NOT NULL,
+    `shipAddressId` INT UNSIGNED NOT NULL,
+    `billAddressId` INT UNSIGNED NOT NULL,
+    `status`        INT UNSIGNED NOT NULL,-- submitted, confirmed, ...
+    PRIMARY KEY (`orderId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `product`(
-   `productId` INT UNSIGNED AUTO_INCREMENT,
-   `itemName` VARCHAR(50) NOT NULL,
-   `parentCategory` INT UNSIGNED NOT NULL,
-   PRIMARY KEY ( `productId` )
-)CHARSET=utf8;
+-- --------------------------------------------------
+-- shopping cart line
+-- --------------------------------------------------
+DROP TABLE IF EXISTS `lineInfo`;
+CREATE TABLE `lineInfo`
+(
+    `lineId`   INT UNSIGNED AUTO_INCREMENT,
+    -- Based on property `status`. In the cart, it store userId. After ordered, it store orderId.
+    `parentId` INT UNSIGNED NOT NULL, -- userId or orderId
+    `status`   INT UNSIGNED NOT NULL, -- In cart: 0; Ordered: 1; ...
+    `itemId`   INT UNSIGNED NOT NULL,
+    `quantity` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`lineId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `item`(
-   `itemId` INT UNSIGNED AUTO_INCREMENT,
-   `listPrice` DECIMAL(10, 2) NOT NULL,
-   `inventory` INT UNSIGNED,
-   PRIMARY KEY ( `itemId` )
-)CHARSET=utf8;
+-- --------------------------------------------------
+-- --------------------------------------------------
+-- pet
+-- --------------------------------------------------
+-- --------------------------------------------------
+DROP TABLE IF EXISTS `categoryInfo`;
+CREATE TABLE `categoryInfo`
+(
+    `categoryId`   INT UNSIGNED AUTO_INCREMENT,
+    `categoryName` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`categoryId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `lineItem`(
-   `lineId` INT UNSIGNED AUTO_INCREMENT,
-   `parentId` INT NOT NULL,  -- Based on property `status`. In the cart, it store userId. After ordered, it store orderId.
-   `status` INT DEFAULT 0,   -- In cart: 0; Ordered: 1; ... (change if need)
-   `itemId` INT NOT NULL,
-   `quantity` INT DEFAULT 1,
-   PRIMARY KEY ( `lineId` )
-)CHARSET=utf8;
+DROP TABLE IF EXISTS `productInfo`;
+CREATE TABLE `productInfo`
+(
+    `productId`   INT UNSIGNED AUTO_INCREMENT,
+    `categoryId`  INT UNSIGNED NOT NULL,
+    `productName` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`productId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE IF NOT EXISTS `order`(
-   `orderId` INT UNSIGNED AUTO_INCREMENT,  -- AUTO_INCREMENT?
-   `orderTime` DATETIME NOT NULL,          -- Should use DATE if time is not necessary
-   `shipAddId` INT NOT NULL,
-   `billAddId` INT NOT NULL,
-   `status` INT DEFAULT 0,                 -- submitted, confirmed, ...
-   PRIMARY KEY ( `orderId` )
-)CHARSET=utf8;
+DROP TABLE IF EXISTS `itemInfo`;
+CREATE TABLE `itemInfo`
+(
+    `itemId`    INT UNSIGNED AUTO_INCREMENT,
+    `productId` INT UNSIGNED NOT NULL,
+    `listPrice` INT UNSIGNED NOT NULL,
+    `inventory` INT UNSIGNED NOT NULL,
+    `attr1`     VARCHAR(100),
+    `attr2`     VARCHAR(100),
+    `attr3`     VARCHAR(100),
+    `attr4`     VARCHAR(100),
+    `attr5`     VARCHAR(100),
+    PRIMARY KEY (`itemId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
