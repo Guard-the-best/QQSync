2ea246c83879a338022545d67d04354b33da4a9a
What-a-mess <44860987+What-a-mess@users.noreply.github.com>
2019-10-31 11:17:54 +0800

    现在有了获取购物车长度的servlet

--- src/main/java/cn/edu/csu/dyp/web/servlet/cartServlet/GetCartLengthServlet.java
+++ src/main/java/cn/edu/csu/dyp/web/servlet/cartServlet/GetCartLengthServlet.java
@@ -1,4 +1,4 @@
-package cn.edu.csu.dyp.web.servlet.skipServlet;
+package cn.edu.csu.dyp.web.servlet.cartServlet;
 
 import cn.edu.csu.dyp.model.user.LineItem;
 import cn.edu.csu.dyp.model.user.User;
@@ -11,22 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet(name = "ToMainServlet")
-public class ToMainServlet extends HttpServlet {
-    private static final String INDEX_PAGE = "/WEB-INF/jsp/AfterLogin/main.jsp";
-
+@WebServlet(name = "GetCartLengthServlet")
+public class GetCartLengthServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        User user = (User)session.getAttribute("user");
-        System.out.println(user.getUserId());
+        User user = (User) session.getAttribute("user");
+        CartService cartService = new CartService();
 
+        List<LineItem> list = cartService.getCart(user.getUserId());
 
-        request.getRequestDispatcher(INDEX_PAGE).forward(request, response);
+        PrintWriter out = response.getWriter();
+        out.print(list.size());
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
+
     }
 }
