21b25cb78af4689048b9f4f1f808629278e00745
int mian <w923959419@gmail.com>
2020-06-13 21:45:46 +0800

    Create CorsConfig.java

---
+++ src/main/java/cn/edu/csu/dyp/Config/CorsConfig.java
@@ -0,0 +1,17 @@
+package cn.edu.csu.dyp.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("*","POST", "GET", "PATCH", "DELETE")
+//                .allowCredentials(true)
+        ;
+    }
+}
